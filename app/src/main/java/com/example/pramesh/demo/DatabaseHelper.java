e411fac23caeb86146deed7bfc34c5dfbed1dad4
Suzal3579 <pe.messh@gmail.com>
2016-11-17 19:45:08 +0530

    Update and delete method added - Work in progress - All things going all fine. UI Edits to be performed

--- app/src/main/java/com/example/pramesh/demo/DatabaseHelper.java
+++ app/src/main/java/com/example/pramesh/demo/DatabaseHelper.java
@@ -11,6 +11,8 @@ import android.os.Message;
 import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
+import static android.R.attr.id;
+
 /**
  * Created by Pramesh Bajracharya on11/11/2016.
  */
@@ -37,7 +39,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             db.execSQL("DROP TABLE "+TABLE_NAME+" IF EXISTS "+TABLE_NAME+";");
     }
     public boolean insertData(String name,String surname,String marks){
-        SQLiteDatabase db = this.getWritableDatabase();               //  Checks ...
+        SQLiteDatabase db = this.getWritableDatabase();               //  database instance
         ContentValues contentValues  = new ContentValues();
         contentValues.put(DatabaseHelper.COL_2,name);
         contentValues.put(DatabaseHelper.COL_3,surname);
@@ -49,9 +51,26 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return true;
         }
     }
-     public Cursor getAllData(){
-         SQLiteDatabase db = this.getWritableDatabase();               //  Checks ...
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();               //  data base instance ...
          Cursor res = db.rawQuery("SELECT * FROM "+TABLE_NAME+" ;",null);
          return res;
-     }
+    }
+
+    public Integer deletedata(String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ?", new String[]{id});
+    }
+
+    public boolean updateData(String id, String name, String surname, String marks) {
+        SQLiteDatabase db = this.getWritableDatabase();               //  data base instance ...
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, id);
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, surname);
+        contentValues.put(COL_4, marks);
+        db.update(TABLE_NAME, contentValues, "ID = ?", new String[]{id});
+        return true;
+    }
 }
--- app/src/main/java/com/example/pramesh/demo/MainActivity.java
+++ app/src/main/java/com/example/pramesh/demo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pramesh.demo;
 
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,8 +12,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper myDb;
-    EditText editName,editSurname,editMarks;
-    Button buttonInsert,buttonView;
+    EditText editName, editSurname, editMarks, editTextId;
+    Button buttonInsert, buttonView, buttonUpdate, buttonDelete;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,21 +22,36 @@ public class MainActivity extends AppCompatActivity {
         editName = (EditText) findViewById(R.id.editText);
         editSurname = (EditText) findViewById(R.id.editText2);
         editMarks = (EditText) findViewById(R.id.editText3);
+        editTextId = (EditText) findViewById(R.id.editTextId);
         buttonInsert = (Button) findViewById(R.id.button_add);
         buttonView = (Button) findViewById(R.id.button_view);
+        buttonUpdate = (Button) findViewById(R.id.update);
+        buttonDelete = (Button) findViewById(R.id.buttonDelete);
         AddData();
         viewData();
+        updateData();
+        deleteData();
     }
     public void AddData(){
         buttonInsert.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                       boolean isInserted = myDb.insertData(editName.getText().toString(),editSurname.getText().toString(),editMarks.getText().toString());
-                        if (isInserted == true){
-                            Toast.makeText(MainActivity.this,"Data Inserted",Toast.LENGTH_LONG).show();
-                        }else {
-                            Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                        if (editName.getText().toString().length() == 0) {
+                            editName.setError("Task 1 is required!");
+                            if (editSurname.getText().toString().length() == 0) {
+                                editSurname.setError("Task 2 is required!");
+                                if (editMarks.getText().toString().length() == 0) {
+                                    editMarks.setError("Task 3 is required!");
+                                }
+                            }
+                        } else {
+                            boolean isInserted = myDb.insertData(editName.getText().toString(), editSurname.getText().toString(), editMarks.getText().toString());
+                            if (isInserted == true) {
+                                Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                            }
                         }
                     }
                 }
@@ -49,11 +65,12 @@ public class MainActivity extends AppCompatActivity {
                     Cursor res = myDb.getAllData();
                     if(res.getCount() == 0){
                         Toast.makeText(MainActivity.this,"Data Not found",Toast.LENGTH_LONG).show();
-                        showMessage("Add Something!","Add Some Task. You currently have to task added.");
+                        showMessage("Add Something!", "Add Some Task. You currently have no task to be displayed.");
                         return;
                     }else{
                         StringBuffer buffer = new StringBuffer();
                         while(res.moveToNext()){
+                            buffer.append("Number :" + res.getString(0) + "\n");
                             buffer.append("Task 1 :"+res.getString(1)+"\n");
                             buffer.append("Task 2 :"+res.getString(2)+"\n");
                             buffer.append("Task 3 :"+res.getString(3)+"\n\n");
@@ -63,6 +80,51 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             }
+        );
+    }
+
+    public void updateData() {
+        buttonUpdate.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (editTextId.getText().toString().length() == 0) {
+                            editTextId.setError("Please enter Number here");
+                        } else {
+                            boolean isUpdated = myDb.updateData(
+                                    editTextId.getText().toString(),
+                                    editName.getText().toString(),
+                                    editSurname.getText().toString(),
+                                    editMarks.getText().toString()
+                            );
+                            if (isUpdated == true) {
+                                Toast.makeText(MainActivity.this, "Data Updated", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(MainActivity.this, "Data Not Updated", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                }
+        );
+    }
+
+    public void deleteData() {
+        buttonDelete.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (editTextId.getText().toString().length() == 0) {
+                            editTextId.setError("Please enter Number here");
+                        } else {
+                            Integer deletedRows = myDb.deletedata(editTextId.getText().toString());
+                            if (deletedRows > 0) {
+                                Toast.makeText(MainActivity.this, "Data Deleted", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(MainActivity.this, "Data Not Deleted. Make sure you have enter the number right.", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                }
         );
     }
 
